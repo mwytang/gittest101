0df8c22c0ead4775b54ffbb1085b9f61ba26abbc
ScottSallinen <scott@anyx.ca>
2013-10-31 13:03:52 -0700

    another cat

--- src/tang/michael/gittest/MainActivity.java
+++ src/tang/michael/gittest/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	// cat cat
+	// ANOTHER CAT
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
