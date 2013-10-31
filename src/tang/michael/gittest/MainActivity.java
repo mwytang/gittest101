c81239bbf64dddc00676397f0de642e370d29009
ScottSallinen <scott@anyx.ca>
2013-10-31 13:53:50 -0700

    pls

--- src/tang/michael/gittest/MainActivity.java
+++ src/tang/michael/gittest/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	// no conflicts pls
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
